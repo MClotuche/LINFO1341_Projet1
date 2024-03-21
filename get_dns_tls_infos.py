eccb3e3cfd3d6dae64137194b09ce099da0ffff9
Martin Clotuche <martinx076@gmail.com>
2024-03-21 12:42:43 +0100

    updated script to analyse TLS - ip - ports

--- get_dns_tls_infos.py
+++ get_dns_tls_infos.py
@@ -29,10 +29,13 @@ dns_query_types = {
 }
 
 
-def extract_dns_packets(pcap_file):
+def extract_dns_tls_packets(pcap_file):
 
     dns_dataset = {}
     Dns_entry = namedtuple('dn', ['ns','ty']) #additionnal records neglected
+    
+    tls_dataset = {}
+    Tls_entry = namedtuple('dn', ['ns','sp','dip'])
     
     # Ouvrir le fichier pcap
     capture = pyshark.FileCapture(pcap_file)
@@ -40,6 +43,8 @@ def extract_dns_packets(pcap_file):
     for packet in capture:
         # Vérifier si le paquet est de type DNS
         if 'DNS' in packet:
+
+            #Récup des infos DNS
             try:
                 name_server = packet.dns.soa_mname
             except:
@@ -48,15 +53,50 @@ def extract_dns_packets(pcap_file):
             # Check si déjà dans le dataset
             if packet.dns.qry_name in dns_dataset:
                 #ajoute uniqment les secondes du temps
-                
                 dns_dataset[packet.dns.qry_name].append(Dns_entry(name_server, dns_query_types[int(packet.dns.qry_type)]))
             else: #ajout
                 dns_dataset[packet.dns.qry_name] = [Dns_entry(name_server, dns_query_types[int(packet.dns.qry_type)])]
+        if 'TLS' in packet:
+            try:
+                handshake = packet.tls.handshake
+            except:
+                handshake = None
+            if handshake:
+                srcport = packet.tcp.srcport
+                #dstport = packet.tcp.dstport
+                #dstport = 443 TOUJOURS - pas nécéssaire
+                try:
+                    domain_name = packet.tls.handshake_extensions_server_name
+                except:
+                    continue
+
+                try: #ipv6 vs ipv4
+                    dstaddr = packet.ipv6.dst
+                    #srcaddr = packet.ipv6.src
+                    #srcaddr = mon adresse TOUJOURS '(pex 2a02:a03f:a629:301:d55a:e041:3cc2:ad46)
+                except:
+                    dstaddr = packet.ip.dst
+                    #srcaddr = packet.ip.src
+                    #srcaddr = mon adresse TOUJOURS '(pex 2a02:a03f:a629:301:d55a:e041:3cc2:ad46)
+
+                tls_dataset[domain_name] = Tls_entry(domain_name, srcport, dstaddr)
+    
 
     # Fermer le fichier pcap
     capture.close()
 
-    return dns_dataset
+    #print le paquet tls_dataset[0] sans pretty_print
+    #print(tls_dataset[0])
+    #autres moyens de print?
+    """
+    for layer in tls_dataset[0].layers:
+        print(layer.layer_name)
+        for field in layer._all_fields:
+            print(field, getattr(layer, field))
+    print("\n")
+    """
+    
+    return dns_dataset, tls_dataset
 
 def main():
     # Vérifier si un fichier pcap est fourni en argument
@@ -70,17 +110,23 @@ def main():
     csv_name = pcap_file.split('/')[-1]
 
     # Extraire les paquets DNS du fichier pcap
-    dns_packets = extract_dns_packets(pcap_file)
+    dns_packets,tls_paquets = extract_dns_tls_packets(pcap_file)
 
     #Parcourt le dictionnaire dns_packets
     #Pour chaque élement de la liste, ajoute une nouvelle ligne dans un fichier excel
     #avec le nom du serveur responsable et le type de requête DNS
+    
     with open('dns_recap/'+str(csv_name)+'.csv', 'w') as file:
         file.write("DNS Request,Name Server,Query Type\n")
         for dns_request, dns_entries in dns_packets.items():
             for entry in dns_entries:
                 file.write(f"{dns_request},{entry.ns},{entry.ty}\n")
-
+    
+    #de même pour les paquets tls
+    with open('tls_recap/'+str(csv_name)+'.csv', 'w') as file:
+        file.write("Domain Name,Source Port,Destination IP\n")
+        for domain_name, tls_entry in tls_paquets.items():
+            file.write(f"{tls_entry.ns},{tls_entry.sp},{tls_entry.dip}\n")
     #Liste des infos acessibles dans un paquet: ici une réponse
     #Récup serveur responsable: dns.resp_name
     """
